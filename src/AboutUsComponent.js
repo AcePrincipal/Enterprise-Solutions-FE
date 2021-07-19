155917b8974f24fe25337bbe418df66fe9973094
AcePrincipal <acepetrov96@GMAIL.com>
2021-07-19 18:07:58 -0500

    Created the AboutUsComponent which has a header and text, that describes the company

---
+++ src/AboutUsComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class AboutUsComponent extends Component {
+  render() {
+    return (
+      <div className="about" id="abt">
+        <h1> About Us!</h1>
+        <p>Welcome to Enterprise Solutions, we are a company based in Houston, Texas, that specialize in important 
+          technical solutions for enterprise businesses. Our specialists have skills that range from software 
+          development, database installations, and establishing local networks. We beleive that our products can 
+          give an organization the assets needed to lead an industry, whether it be in accounting, finanzing, 
+          medicine, or technology, we'll give you the advantages YOU NEED. 
+        </p>
+      </div>
+    );
+  }
+}
--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import WelcomeComponent from './WelcomeComponent';
-// import AboutUsComponent from './AboutUsComponent';
+import AboutUsComponent from './AboutUsComponent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <WelcomeComponent/>
-        {/* <AboutUsComponent/> */}
+        <AboutUsComponent/>
       </div>
     );
   }
