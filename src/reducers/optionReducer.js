5c5f85cf4483721970ef3001b1097185a3a7c2d4
AcePrincipal <acepetrov96@GMAIL.com>
2021-07-21 19:05:40 -0500

    Set up provider, store, and created a reducer

---
+++ src/reducers/optionReducer.js
@@ -0,0 +1,10 @@
+function optionReducer(state = {}, action){
+    switch(action.type){
+        case "A":
+            return currentState = true;
+        default:
+            return currentState;
+    }
+}
+
+export default optionReducer;
